20d299bb1bc49b3e201be058b7d9974f6777d575
annvolfers <annvolfers@me.com>
2022-10-10 22:15:00 +0300

    Добавляет валидацию части полей формы

--- js/form.js
+++ js/form.js
@@ -1,5 +1,20 @@
 const mapFilters = document.querySelector('.map__filters');
 const adForm = document.querySelector('.ad-form');
+const roomNumber = adForm.querySelector('#room_number');
+const capacity = adForm.querySelector('#capacity');
+
+const accommodationOption = {
+  '1': ['1'],
+  '2': ['2', '1'],
+  '3': ['3', '2', '1'],
+  '100': ['0'],
+};
+
+const pristine = new Pristine(adForm, {
+  classTo: 'ad-form__element',
+  errorClass: 'ad-form__element--invalid',
+  errorTextParent: 'ad-form__element',
+});
 
 function deactivatePage() {
   mapFilters.classList.add('map__filters--disabled');
@@ -24,6 +39,24 @@ function activatePage() {
     item.disabled = false;
   }
 }
+
+function validateAccommodation() {
+  return accommodationOption[roomNumber.value].includes(capacity.value);
+}
+
+function getAccommodationErrorMessage() {
+  return 'Выбранное количество гостей недопустимо для выбранного количества комнат';
+}
+
+pristine.addValidator(roomNumber, validateAccommodation, getAccommodationErrorMessage);
+
+capacity.addEventListener('change', () => pristine.validate(roomNumber));
+
+adForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  pristine.validate();
+});
 
 deactivatePage();
 activatePage();
