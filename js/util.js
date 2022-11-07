82ed9ceca00db8925c3b119396e81ee376d6c077
annvolfers <annvolfers@me.com>
2022-11-07 16:39:06 +0300

    Убирает неиспользуемые функции

--- js/util.js
+++ js/util.js
@@ -1,36 +1,3 @@
-function getRandomPositiveInteger(min, max) {
-  if (min < 0 || max < 0 || typeof min !== 'number' || typeof max !== 'number') {
-    return NaN;
-  }
-
-  const lower = Math.ceil(Math.min(min, max));
-  const greater = Math.floor(Math.max(min, max));
-
-  return Math.floor(Math.random() * (greater - lower + 1)) + lower;
-}
-
-function getRandomGeographicCoordinate(min, max, decimals) {
-  if (min < 0 || max < 0 || decimals < 0 || typeof min !== 'number' || typeof max !== 'number' || typeof decimals !== 'number') {
-    return NaN;
-  }
-
-  const lower = Math.min(min, max);
-  const greater = Math.max(min, max);
-
-  return Number((Math.random() * (greater - lower) + lower).toFixed(decimals));
-}
-
-const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
-
-function createIdGenerator() {
-  let lastGeneratedId = 0;
-
-  return function () {
-    lastGeneratedId++;
-    return lastGeneratedId;
-  };
-}
-
 function declineNumerals(value, words) {
   const remainderOfDivisionBy100 = Math.abs(value) % 100;
   const remainderOfDivisionBy10 = remainderOfDivisionBy100 % 10;
@@ -63,4 +30,4 @@ function debounce(callback, timeoutDelay = 500) {
   };
 }
 
-export { getRandomPositiveInteger, getRandomGeographicCoordinate, getRandomArrayElement, createIdGenerator, declineNumerals, enableElement, disableElement, debounce };
+export { declineNumerals, enableElement, disableElement, debounce };
